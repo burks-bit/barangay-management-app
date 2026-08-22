2b90a95816a61cc921268e98d6075cbb5463af81
Bert <bertnet08@gmail.com>
2026-08-22 10:40:11 +0800

    Second Commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="csrf-token" :content="{{ csrf_token() }}" />
-        <title inertia>{{ config('app.name', 'Barangay Management System') }}</title>
+        <title inertia>{{ config('app.name', 'Barangay Management and Community Services System') }}</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @inertiaHead
     </head>
